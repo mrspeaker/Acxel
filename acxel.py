55a83cc0f417e55c3af62e991a5ca5bf289e7c97
Mr Speaker <mrspeaker@gmail.com>
2015-01-15 18:08:03 -0500

    better example, yo

--- acxel.py
+++ acxel.py
@@ -14,6 +14,7 @@ class AcxelCommand(sublime_plugin.TextCommand):
 		w = max([len(line) for line in lines])
 		h = len(lines)
 
+		# Let's get all JavaScript-y!
 		makey = ""
 		makey += "var can = document.createElement('canvas'), c = can.getContext('2d'),"
 		makey += "data = c.createImageData(" + str(w) + ", " + str(h) + "), i = 0;"
@@ -30,11 +31,18 @@ class AcxelCommand(sublime_plugin.TextCommand):
 		makey += "c.putImageData(data, 0, 0); "
 		makey += "document.body.appendChild(c.canvas);"
 
+		# to the clipboard!
 		sublime.set_clipboard(makey)
 
-# ********
-# *      *
-# ***  ***
-#   *  *
-#   *  *
-#   ****
+
+#   __   __   ______       _______   _______   _______   _______   ___   _   _______   ______
+#  |  |_|  | |      |     |       | |       | |       | |       | |   | | | |       | |      |
+#  |       | |    _ |     |       | |       | |       | |   _   | |   | | | |       | |    _ |
+#  |       | |   | ||     |  _____| |    _  | |    ___| |  | |  | |   |_| | |    ___| |   | ||
+#  |       | |   |_||_    | |_____  |   |_| | |   |___  |  |_|  | |      _| |   |___  |   |_||_
+#  |       | |        |   |       | |       | |       | |       | |     |   |       | |        |
+#  |       | |    __  |   |_____  | |    ___| |    ___| |       | |     |_  |    ___| |    __  |
+#  | ||_|| | |   |  | |    _____| | |   |     |   |___  |   _   | |    _  | |   |___  |   |  | |
+#  | |   | | |   |  | |   |       | |   |     |       | |  | |  | |   | | | |       | |   |  | |
+#  | |   | | |   |  | |   |       | |   |     |       | |  | |  | |   | | | |       | |   |  | |
+#  |_|   |_| |___|  |_|   |_______| |___|     |_______| |__| |__| |___| |_| |_______| |___|  |_|
